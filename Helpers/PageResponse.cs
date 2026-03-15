ff3e483561346a0cc8f763650c2e62393356002f
mahjabin-mim <mahjabinmim711@gmail.com>
2026-03-15 09:15:03 +0600

    pagination response formate updated

--- Helpers/PageResponse.cs
+++ Helpers/PageResponse.cs
@@ -2,11 +2,13 @@ namespace ProductValidation.Helpers
 {
     public class PageResponse<T>
     {
+        public List<T> Data { get; set; }
         public int PageNumber { get; set; }
         public int PageSize { get; set; }
         public int TotalRecords { get; set; }
         public int TotalPages { get; set; }
-        public List<T> Data { get; set; }
+        public bool HasPrevious { get; set; }
+        public bool HasNext { get; set; }
 
         public PageResponse(List<T> data, int count, int pageNumber, int pageSize)
         {
@@ -14,7 +16,10 @@ namespace ProductValidation.Helpers
             TotalRecords = count;
             PageNumber = pageNumber;
             PageSize = pageSize;
+
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            HasPrevious = pageNumber > 1;
+            HasNext = pageNumber < TotalPages;
         }
     }
 }
